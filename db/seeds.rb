cc2000c3fbbaa3251ba7e2ce02adcaf314ba2841
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-18 11:48:16 -0700

    seeded db and added a gitignore file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying old data...'
+User.destroy_all
+Ingredient.destroy_all
+UserIngredient.destroy_all
+
+puts 'Creating users...'
+15.times {User.create(username: Faker::Internet.username, password_digest: Faker::Alphanumeric.alphanumeric(number: 10), favorite_food: Faker::Food.dish, bio: Faker::Books::Dune.quote)}
+
+puts 'Creating ingredients...'
+30.times {Ingredient.create(name: Faker::Food.ingredient)}
+
+puts "Filling user's fridges..."
+40.times {UserIngredient.create(user: User.all.sample, ingredient: Ingredient.all.sample)}
+
+puts 'Seeding complete.'
