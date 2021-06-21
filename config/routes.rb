c506da4a971426f98ba6373c4355b309de4d2f89
Chau Tran <cctran96@gmail.com>
2021-06-21 12:25:54 -0700

    allow users to stay logged in using JWT

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       post '/login', to: 'authentication#login'
+      get '/login', to: 'authentication#user'
+      get '/usernames', to: 'users#usernames'
       get '/profile', to: 'users#profile'
     end
   end
