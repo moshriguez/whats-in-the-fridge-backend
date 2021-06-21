1884b902b59a37da46d3ef751b7517f6f00ad539
Zach Frohardt <zfrohardt@gmail.com>
2021-06-21 10:59:42 -0700

    fixed formatting, delete order, and added sane passwords to user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
 puts 'Destroying old data...'
+UserIngredient.destroy_all
 User.destroy_all
 Ingredient.destroy_all
-UserIngredient.destroy_all
 
 puts 'Creating users...'
-15.times {User.create(username: Faker::Internet.username, password_digest: Faker::Alphanumeric.alphanumeric(number: 10), favorite_food: Faker::Food.dish, bio: Faker::Books::Dune.quote)}
+15.times do
+    username = Faker::Internet.username
+    User.create(username: username, password_digest: BCrypt::Password::create(username), favorite_food: Faker::Food.dish, bio: Faker::Books::Dune.quote)
+end
 
 puts 'Creating ingredients...'
-30.times {Ingredient.create(name: Faker::Food.ingredient)}
+30.times do
+    Ingredient.create(name: Faker::Food.ingredient)
+end
 
 puts "Filling user's fridges..."
-40.times {UserIngredient.create(user: User.all.sample, ingredient: Ingredient.all.sample)}
+40.times do
+    UserIngredient.create(user: User.all.sample, ingredient: Ingredient.all.sample)
+end
 
 puts 'Seeding complete.'
