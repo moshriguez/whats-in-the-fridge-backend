497d94cbbd8ce5bbc12a25b3c0643f7108cdbebc
Chau Tran <cctran96@gmail.com>
2021-06-22 14:24:59 -0700

    created custom serializer attrbiute

--- app/serializers/user_ingredient_serializer.rb
+++ app/serializers/user_ingredient_serializer.rb
@@ -1,5 +1,9 @@
 class UserIngredientSerializer < ActiveModel::Serializer
     attributes :id, :ingredient
     belongs_to :ingredient
+
+    def ingredient
+      Ingredient.select("id, name").find(object.ingredient_id)
+    end
   end
   
