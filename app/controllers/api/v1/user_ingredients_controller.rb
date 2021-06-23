d6dc4191f2f9d057881b20f1b3b9d017755d4dca
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-06-22 18:09:57 -0700

    added return to destory action

--- app/controllers/api/v1/user_ingredients_controller.rb
+++ app/controllers/api/v1/user_ingredients_controller.rb
@@ -7,5 +7,6 @@ class Api::V1::UserIngredientsController < ApplicationController
     def destroy
         user_ingredient = UserIngredient.find(params[:id])
         user_ingredient.destroy
+        render json: {user_ingredient: user_ingredient}
     end
 end
